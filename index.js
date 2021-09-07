81b078d4fc1a8b97b4ca425ebf984766f5273b6f
GBSourceCodes <gaurangbhatt19@gmail.com>
2021-09-08 00:35:22 +0530

    dish_router leaders_router promo_router

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const dishrouter=require("./routes/dishRouter");
+const leaderrouter=require("./routes/leaderRouter");
+const promorouter=require("./routes/promoRouter");
+
+const express= require('express');
 const app = express();
-const port=3000 || process.env.PORT;
-
-
+const port=3030 || process.env.PORT;
+app.use("",dishrouter);
+app.use("",leaderrouter);
+app.use("",promorouter);
 app.listen(port,()=>{
     console.log("Server Started");
 })
--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -1,15 +1,18 @@
 const express= require('express');
-
-const router=express.router();
+const router=express.Router();
 router.route("/dishes/:dishId").get((req,res)=>{
+    res.send("Dish ID: "+req.params.dishId);
+
 
 }).post((req,res)=>{
-
+    res.send("Added Dish ID: "+req.params.dishId);
 
 }).delete((req,res)=>{
+    res.send(" Deleting Dish of ID: "+req.params.dishId);
 
 }).put((req,res)=>{
+    res.send("Updating Dish ID to : "+req.params.dishId);
 
 });
 
-export default router;
+module.exports=router;
